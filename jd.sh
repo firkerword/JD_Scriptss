9875767300894d89c4f1e9cb4a286bdbe01099dc
firkerword <136000981@qq.com>
2021-07-19 06:58:07 +0000

    github_actions

--- jd.sh
+++ jd.sh
@@ -1277,7 +1277,7 @@ checklog() {
 	for i in `cat $log2`
 	do
 		echo "#### ${i}详细的错误" >> $log3
-		grep -E  "错误|失败|module" $i | grep -v '京东天天\|京东商城\|京东拍拍\|京东现金\|京东秒杀\|京东日历\|京东金融\|京东金贴\|金融京豆\|检测\|参加团主\|参团失败\|node_modules\|sgmodule\|无助力机会\|不可以为自己助力\|助力次数耗尽\|礼包已经抢完\|限流严重\|不能去好友工厂打工啦' | sort -u >> $log3
+		grep -E  "错误|失败|module" $i | grep -v '京东天天\|京东商城\|京东拍拍\|京东现金\|京东秒杀\|京东日历\|京东金融\|京东金贴\|金融京豆\|检测\|参加团主\|参团失败\|node_modules\|sgmodule\|无助力机会\|不可以为自己助力\|助力次数耗尽\|礼包已抢完\|限流严重\|不能去好友工厂打工啦' | sort -u >> $log3
 	done
 
 	if [ $num = "no_error" ]; then
@@ -1479,9 +1479,18 @@ backnas() {
 	echo -e "$green>> 开始备份到nas$white"
 	sleep 5
 
+	echo -e "$green>> 打包前处理，删除ccr_js文件"
+	rm -rf $back_file_patch/JD_Script/ccr_js/*
+	echo -e "$green>> 删除完成$white"
+	sleep 5
+
 	echo -e "$green>> 开始打包文件$white"
 	tar -zcvf /tmp/$back_file_name $back_file_patch
 	sleep 5
+
+	echo -e "$green>> 开始恢复ccr_js文件夹$white"
+	update
+	echo "$green>>  恢复完成$white"
 
 	clear
 	echo -e "$green>> 开始上传文件 $white"
@@ -2191,6 +2200,9 @@ additional_settings() {
 
 	rm -rf $dir_file_js/app.*.js
 
+	#默认关闭京东到家通知
+	sed -i "s/isNotify = true/isNotify = false/g" $ccr_js_file/$i/jddj_fruit.js
+
 }
 
 if [ ! `cat /tmp/github.txt` == "firkerword" ];then 
@@ -2212,17 +2224,15 @@ close_notification() {
 		echo -e "$green今天周一不关闭农场萌宠通知$white"
 	else
 		case `date +%H` in
-		22|23|00|01|02|03)
+		22|23|00|01)
 			for i in `ls $ccr_js_file | grep -E "^js"`
 			do
 				sed -i "s/jdNotify = true/jdNotify = false/g" $ccr_js_file/$i/jd_fruit.js
 				sed -i "s/jdNotify = true/jdNotify = false/g" $ccr_js_file/$i/jd_pet.js
-				sed -i "s/isNotify = false/isNotify = true/g" $ccr_js_file/$i/jddj_fruit.js
 			done
-
 			sed -i "s/jdNotify = true/jdNotify = false/g" $dir_file_js/jd_fruit.js
 			sed -i "s/jdNotify = true/jdNotify = false/g" $dir_file_js/jd_pet.js
-			sed -i "s/isNotify = false/isNotify = true/g" $dir_file_js/jddj_fruit.js
+
 			echo -e "$green暂时不关闭农场和萌宠通知$white"
 		;;
 		*)
@@ -2230,13 +2240,11 @@ close_notification() {
 			do
 				sed -i "s/jdNotify = false/jdNotify = true/g" $ccr_js_file/$i/jd_fruit.js
 				sed -i "s/jdNotify = false/jdNotify = true/g" $ccr_js_file/$i/jd_pet.js
-				sed -i "s/isNotify = true/isNotify = false/g" $ccr_js_file/$i/jddj_fruit.js
 			done
 
 			sed -i "s/jdNotify = false/jdNotify = true/g" $dir_file_js/jd_fruit.js
 			sed -i "s/jdNotify = false/jdNotify = true/g" $dir_file_js/jd_pet.js
-			sed -i "s/isNotify = true/isNotify = false/g" $dir_file_js/jddj_fruit.js
-			echo -e "$green时间大于凌晨三点开始关闭农场和萌宠通知$white"
+			echo -e "$green时间大于凌晨一点开始关闭农场和萌宠通知$white"
 		;;
 		esac
 	fi
